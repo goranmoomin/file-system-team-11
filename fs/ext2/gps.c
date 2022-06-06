2749c68b144a527960afe6bed91cf22cebd71aa0
WallabyG <ycm1366@gmail.com>
2022-06-06 15:47:09 +0900

    Fix unmount & concurrency issue

--- fs/ext2/gps.c
+++ fs/ext2/gps.c
@@ -22,11 +22,13 @@ int ext2_set_gps_location(struct inode *inode)
 
 	spin_lock(&curloc_lock);
 
+	spin_lock(&inode->i_lock);
 	ei->i_lat_integer = curloc.lat_integer;
 	ei->i_lat_fractional = curloc.lat_fractional;
 	ei->i_lng_integer = curloc.lng_integer;
 	ei->i_lng_fractional = curloc.lng_fractional;
 	ei->i_accuracy = curloc.accuracy;
+	spin_unlock(&inode->i_lock);
 
 	spin_unlock(&curloc_lock);
 
@@ -38,11 +40,13 @@ int ext2_get_gps_location(struct inode *inode,
 {
 	struct ext2_inode_info *ei = EXT2_I(inode);
 
+	spin_lock(&inode->i_lock);
 	gps_location->lat_integer = ei->i_lat_integer;
 	gps_location->lat_fractional = ei->i_lat_fractional;
 	gps_location->lng_integer = ei->i_lng_integer;
 	gps_location->lng_fractional = ei->i_lng_fractional;
 	gps_location->accuracy = ei->i_accuracy;
+	spin_unlock(&inode->i_lock);
 
 	return 0;
 }
--- kernel/gps.c
+++ kernel/gps.c
@@ -7,31 +7,31 @@
 struct gps_location curloc = {};
 DEFINE_SPINLOCK(curloc_lock);
 
-static inline is_valid_gps_location(struct gps_location *loc)
+static inline int is_valid_gps_location(struct gps_location *loc)
 {
 	if (loc->lat_integer == 90 && loc->lat_fractional != 0) {
-		return false;
+		return 0;
 	}
 
 	if (loc->lng_integer == 180 && loc->lng_fractional != 0) {
-		return false;
+		return 0;
 	}
 
 	if (!(-90 <= loc->lat_integer && loc->lat_integer <= 90 &&
 	      -180 <= loc->lng_integer && loc->lng_integer <= 180)) {
-		return false;
+		return 0;
 	}
 
 	if (!(0 <= loc->lat_fractional && loc->lat_fractional < 1000000 &&
 	      0 <= loc->lng_fractional && loc->lng_fractional < 1000000)) {
-		return false;
+		return 0;
 	}
 
 	if (!(0 <= loc->accuracy && loc->accuracy < 1000)) {
-		return false;
+		return 0;
 	}
 
-	return true;
+	return 1;
 }
 
 SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc)
@@ -63,7 +63,7 @@ SYSCALL_DEFINE2(get_gps_location, const char __user *, pathname,
 	struct path path;
 	struct gps_location buf;
 	int (*get_gps_location)(struct inode *, struct gps_location *);
-	int ret;
+	int ret = 0;
 
 	if (!pathname || !loc) {
 		return -EINVAL;
@@ -75,14 +75,18 @@ SYSCALL_DEFINE2(get_gps_location, const char __user *, pathname,
 
 	if (!(get_gps_location =
 		      path.dentry->d_inode->i_op->get_gps_location)) {
-		return -ENODEV;
+		ret = -ENODEV;
+		goto out;
 	}
 
 	get_gps_location(path.dentry->d_inode, &buf);
 
 	if (copy_to_user(loc, &buf, sizeof(buf))) {
-		return -EFAULT;
+		ret = -EFAULT;
+		goto out;
 	}
 
-	return 0;
+out:
+	path_put(&path);
+	return ret;
 }
